94bf6e037330588eb92916e0039bcc6b130cf2fc
Prokhor Minin <len0xx@icloud.com>
2023-08-28 12:16:23 +0500

    Interactive button variant

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -14,11 +14,15 @@
     on:blur
     class={ [ className, variantClass ].filter((c) => !!c.length).join(' ') }
 >
-    <slot />
+    <span class="text-wrapper">
+        <slot />
+    </span>
 </button>
 
 <style>
     button {
+        display: inline-block;
+        position: relative;
         background-color: var(--green);
         color: black;
         border-radius: var(--radius);
@@ -28,6 +32,11 @@
         transition: 0.1s ease-in-out;
         font-size: var(--text-size);
         font-family: 'Gilroy', sans-serif;
+        transition: 0.1s ease-in-out;
+    }
+    button > .text-wrapper {
+        position: relative;
+        z-index: 10;
     }
 
     button:hover {
@@ -40,5 +49,45 @@
 
     button.purple-btn:hover {
         background-color: var(--darker-purple);
+    }
+    button.arrow-variant {
+        padding: 1.5em 6em;
+    }
+    button.arrow-variant::before {
+        content: '';
+        display: block;
+        position: absolute;
+        top: 0.4em;
+        left: 0.4em;
+        height: calc(100% - 0.8em);
+        border-radius: 100em;
+        width: 3.4em;
+        padding: 1em 1.7em;
+        background-color: var(--dark);
+        box-sizing: border-box;
+        transition: 0.2s ease-in-out;
+        z-index: 9;
+    }
+    button.arrow-variant::after {
+        content: '';
+        display: block;
+        position: absolute;
+        top: 0.4em;
+        left: 0.4em;
+        height: calc(100% - 0.8em);
+        border-radius: 100em;
+        padding: 1em 1.7em;
+        box-sizing: border-box;
+        background-image: url("/img/icons/arrow.svg");
+        background-repeat: no-repeat;
+        background-size: 36%;
+        background-position: center;
+        z-index: 11;
+    }
+    button.arrow-variant:hover {
+        color: white;
+    }
+    button.arrow-variant:hover::before {
+        width: calc(100% - 0.8em);
     }
 </style>
