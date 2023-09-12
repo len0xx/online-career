c3279afc9a0c7e7b862e5b9aa5f143cbcb9b9d7e
Prokhor Minin <len0xx@icloud.com>
2023-09-12 23:08:24 +0500

    Add interactivity to Emote component

--- src/lib/components/Emote.svelte
+++ src/lib/components/Emote.svelte
@@ -5,7 +5,10 @@
 </script>
 
 <div class="emote">
-    <Card color="grey"><img src={ image } alt="Emote icon" width="100" height="100" /></Card>
+    <Card color="grey" className="image-wrapper">
+        <img class="primary-emote" src={ image } alt="Emote icon" width="100" height="100" />
+        <img class="secondary-emote" src="/img/emotes/chert.png" alt="Chert icon" width="100" height="100" />
+    </Card>
     <p class="emote-caption align-center"><slot /></p>
 </div>
 
@@ -17,9 +20,36 @@
         width: 100%;
     }
 
-    :global(.emote > .card) {
+    :global(.emote > .card.image-wrapper:not(#a)) {
+        display: grid;
         width: 120px;
         height: 120px;
+    }
+
+    :global(.emote > .card.image-wrapper:not(#a) > img) {
+        transition: 0.1s ease-in-out;
+        grid-row: 1;
+        grid-column: 1;
+    }
+
+    :global(.emote > .card.image-wrapper:not(#a) > img.primary-emote) {
+        opacity: 1;
+        transform: rotateX(0deg);
+    }
+
+    :global(.emote > .card.image-wrapper:not(#a) > img.secondary-emote) {
+        opacity: 0;
+        transform: rotateX(180deg);
+    }
+
+    :global(.emote:hover > .card.image-wrapper:not(#a) > img.primary-emote) {
+        opacity: 0;
+        transform: rotateX(180deg);
+    }
+
+    :global(.emote:hover > .card.image-wrapper:not(#a) > img.secondary-emote) {
+        opacity: 1;
+        transform: rotateX(0deg);
     }
 
     :global(.emote > .card) {
